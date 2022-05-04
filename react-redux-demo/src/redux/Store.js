41802cba94e90d9d1a18475c8a922a5a881961ae
priyabratagig <priyabratachattaraj23@gmail.com>
2022-05-05 03:10:43 +0530

    Apply middleware redux-logger

--- react-redux-demo/src/redux/Store.js
+++ react-redux-demo/src/redux/Store.js
@@ -1,5 +1,6 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
+import logger from 'redux-logger'
 // import reducer from './Cake/Reducers'
 import rootReducer from './RootReducer';
-const Store = createStore(rootReducer);
+const Store = createStore(rootReducer, applyMiddleware(logger));
 export default Store;
